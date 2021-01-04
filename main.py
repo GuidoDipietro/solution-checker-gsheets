d89633d7e1ad0f5286a8f06800cb5676b1beda6b
Guido Dipietro <dipietroguido@gmail.com>
2021-01-04 16:37:37 -0300

    New route for genning 6 digit ints

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from flask import Flask, jsonify
 from threading import Thread
 from cube import Cube
+import random
 
 app = Flask('')
 cube = Cube()
+
+@app.route('/code')
+def code():
+  return jsonify({
+    "code": random.randint(100000, 999999)
+  })
 
 @app.route('/<string:scr>/<string:sol>', methods=['GET'])
 def home(scr, sol):
