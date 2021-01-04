242aa4e715ab36f9de5067328df3f80039b4d777
Guido Dipietro <48221146+GuidoDipietro@users.noreply.github.com>
2021-01-04 01:16:08 -0300

    support for the despicable ’

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ cube = Cube()
 
 @app.route('/<string:scr>/<string:sol>', methods=['GET'])
 def home(scr, sol):
-  scr = scr.replace("%20", " ").replace("%27", "''")
-  sol = sol.replace("%20", " ").replace("%27", "''")
+  scr = scr.replace("%20", " ").replace("%27", "'").replace("’", "'")
+  sol = sol.replace("%20", " ").replace("%27", "'").replace("’", "'")
   return jsonify({
     "result": str(cube.check(scr, sol))
   })
 
 def run():
-    app.run(host='0.0.0.0',port=8080)
+	app.run(host='0.0.0.0',port=8080)
 
 t = Thread(target=run)
 t.start()
